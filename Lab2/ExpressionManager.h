8559830b792647b3fccff3787aaff190324aeb90
Matt Vance <mattwvance@gmail.com>
2020-05-05 17:13:57 -0600

    Lab2 work

--- Lab2/ExpressionManager.h
+++ Lab2/ExpressionManager.h
@@ -1,2 +1,17 @@
+#pragma once
+#include "ExpressionManagerInterface.h"
 #include <iostream>
 #include <string>
+#include <stack>
+
+using namespace std;
+
+class ExpressionManager: public ExpressionManagerInterface {
+public:
+    ExpressionManager() {};
+    virtual ~ExpressionManager() {};
+    bool isBalanced(string expression);
+    string postfixToInfix(string postfixExpression);
+	string postfixEvaluate(string postfixExpression);
+	string infixToPostfix(string infixExpression);
+};
---
+++ Lab2/ExpressionManger.cpp
@@ -0,0 +1,19 @@
+#include "ExpressionManager.h"
+// ExpressionManager() {}
+// virtual ~ExpressionManager() {}
+bool ExpressionManager::isBalanced(string expression) {
+    cout << "isBalanced" << endl;
+    return true;
+};
+string ExpressionManager::postfixToInfix(string postfixExpression) {
+    cout << "postfixToInfix" << postfixExpression << endl;
+    return "a string";
+};
+string ExpressionManager::postfixEvaluate(string postfixExpression) {
+    cout << "postfixEvaluate" << postfixExpression << endl;
+    return "a string";
+};
+string ExpressionManager::infixToPostfix(string infixExpression) {
+    cout << "infixToPostfix" << infixExpression << endl;
+    return "a string";
+};
