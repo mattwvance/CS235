25c1d82fabc3a1e41a176ffc9f4d8dba5ed90453
mattwvance <mattwvance@gmail.com>
2020-05-08 17:16:51 -0600

    it works. find a fun text

--- Lab3_new/main.cpp
+++ Lab3_new/main.cpp
@@ -10,18 +10,6 @@
 using namespace std;
 int main(int argc, char *argv[])
 {
-    // Sets
-    set <string> strset;
-    strset.insert("bug");
-    strset.insert("horse");
-    strset.insert("horse");
-    set<string>::iterator findit;
-    findit = strset.find("horse");
-    cout << "Found horse "<< *findit<<endl;
-    for (set<string>::iterator it=strset.begin(); it!=strset.end(); ++it)
-        cout << ' ' << *it;
-    cout << endl;
-    
     vector<string> tokens;
     set <string> unique;
     string next_line;  // Each data line
@@ -41,42 +29,115 @@ int main(int argc, char *argv[])
 		    // cout << token<<endl;
 	    }
     }
-    cout << "Number of words "<<tokens.size()<<endl;
-    cout << "Number of unique words "<<unique.size()<<endl;
-    for (set<string>::iterator it=unique.begin(); it!=unique.end(); ++it)
-        cout << ' ' << *it;
-    cout << endl;
-    
-    string state = "";
-    map<string, string> wordmap;
-    string last="";
-    for (vector<string>::iterator it=tokens.begin(); it!=tokens.end(); it++) {
-        wordmap[last]=*it;
-        last = *it;
+
+    // This code works
+
+    // map<string, vector<string>> wordmap;
+    // string state = "";
+    // for(vector<string>::iterator it=tokens.begin(); it !=tokens.end(); it++) {
+    //     wordmap[state].push_back(*it);
+    //     state = *it;
+    // }
+    // state = "";
+    // srand(time(0));
+    // for (int i = 0; i < 100; i++) {
+    //     int ind = rand() % wordmap[state].size();
+    //     cout << wordmap[state][ind] << " ";
+    //     state = wordmap[state][ind];
+    // }
+    // cout << endl;
+
+    map<list<string>, vector<string>> wordmap;
+    list<string> state;
+    for (int i = 0; i < 2; ++i) {
+        state.push_back("");
     }
-    for (int i = 0; i < 100; i++) {
-        cout << wordmap[state] << " ";
-        state = wordmap[state];
-    }
-    map<list<string>, vector<string>> wordmap1;
-    list<string> stateList;
-    for (int i = 0; i < 100; i++) {
-        stateList.push_back("");
-    }
-                        
-    for (vector<string>::iterator it=tokens.begin(); it!=tokens.end(); it++) {
-        wordmap1[stateList].push_back(*it);
-        stateList.push_back(*it);
-        stateList.pop_front();
+    for (vector<string>::iterator it=tokens.begin(); it!=tokens.end(); ++it) {
+        wordmap[state].push_back(*it);
+        state.push_back(*it);
+        state.pop_front();
     }
     state.clear();
-    for (int i = 0; i < 100; i++) {
-        stateList.push_back("");
+    for(int i = 0; i < 2; ++i) {
+        state.push_back("");
     }
-    for (int i = 0; i < 100; i++) {
-        int ind = rand() % wordmap[state].size();
-        cout << wordmap1[stateList][ind] << " ";
-        stateList.push_back(wordmap1[stateList][ind]);
-        stateList.pop_front();
+    srand(time(0));
+    for (int i = 0; i < 100; ++i) {
+        int ind= rand() % wordmap[state].size();
+        cout << wordmap[state][ind] << " ";
+        state.push_back(wordmap[state][ind]);
+        state.pop_front();
     }
+    cout << endl;
+
+
+
+
+    // cout << "Number of words "<<tokens.size()<<endl;
+    // cout << "Number of unique words "<<unique.size()<<endl;
+    // map<list<string>, vector<string>> wordmap;
+    // list<string> state;
+    // for (int i = 0; i < 100; ++i) {
+    //     state.push_back("");
+    // }
+    // for (vector<string>::iterator it = tokens.begin(); it != tokens.end(); it++)
+    // {
+    //     state.push_back(*it);
+    // }
+    // srand(time(0));
+    // for (vector<string>::iterator it = tokens.begin(); it != tokens.end(); it++) {
+    //     wordmap[state].push_back(*it);
+    //     state.push_back(*it);
+    //     state.pop_front();
+    // }
+    // cout << " here" << " ";
+    // cout << wordmap[state].size();
+    // state.clear();
+    // for (int i = 0; i < 100; ++i) {
+    //     state.push_back(" ");
+    // }
+    // for (int i = 0; i < 100; ++i) {
+    //     int ind = rand() % wordmap[state].size();
+    //     cout << wordmap[state][ind] << " ";
+    //     state.push_back(wordmap[state][ind]);
+    //     state.pop_front();
+    // }
+    // for (set<string>::iterator it=unique.begin(); it!=unique.end(); ++it)
+    //     cout << ' ' << *it;
+    // cout << endl;
+    
+    // string last="";
+    // for (vector<string>::iterator it=tokens.begin(); it!=tokens.end(); it++) {
+    //     wordmap[last]=*it;
+    //     last = *it;
+    // }
+    // for (int i = 0; i < 100; i++) {
+    //     cout << wordmap[state] << " ";
+    //     state = wordmap[state];
+    // }
+    // cout << endl;
+    // map<list<string>, vector<string>> wordmap;
+    // list<string> state;
+    // for (int i = 0; i < 100; i++) {
+    //     state.push_back("");
+    // }
+                        
+    // for (vector<string>::iterator it=tokens.begin(); it!=tokens.end(); it++) {
+    //     wordmap[state].push_back(*it);
+    //     state.push_back(*it);
+    //     state.pop_front();
+    // }
+    // state.clear();
+    // for (int i = 0; i < 100; i++) {
+    //     state.push_back("");
+    // }
+    // srand(time(0));
+    // for (int i = 0; i < 100; i++) {
+    //     int ind = rand() % wordmap[state].size();
+    //     cout << ind << wordmap[state].size();
+    //     cout << wordmap[state][ind] << " ";
+    //     state.push_back(wordmap[state][ind]);
+    //     state.pop_front();
+    // }
+    // cout << endl;
 }
