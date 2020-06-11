a386524a40acb3a1ea7125427bcd8cd51a2ac0bc
mattwvance <mattwvance@gmail.com>
2020-06-11 11:56:43 -0600

    woops

---
+++ Lab9/Hashmap.cpp
@@ -0,0 +1,13 @@
+#include "Hashmap.h"
+
+
+Hashmap::Hashmap();
+Hashmap::~Hashmap();
+void Hashmap::insert(string key, int value);
+bool Hashmap::contains(string key) const;
+int Hashmap::get(string key) const;
+int Hashmap::&operator[](string key);
+bool Hashmap::remove(string key);
+void Hashmap::clear();
+string Hashmap::toString() const;
+int Hashmap::size() const;
